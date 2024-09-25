3e6486bed4edd8905d8ab9dd493074506ddff293
FernandoVN98 <38290736+FernandoVN98@users.noreply.github.com>
2024-09-25 10:17:13 +0200

    Update run_csvm_train.sh

--- run_csvm_train.sh
+++ run_csvm_train.sh
@@ -4,4 +4,4 @@
 runcompss \
     --pythonpath=$(pwd) \
     --python_interpreter=python3 \
-    /home/user/train_csvm_dislib.py $1 &> >(tee output.log)
+    /home/user/train_csvm_dislib.py $1 $2 $3 $4 $5 $6 &> >(tee output.log)
